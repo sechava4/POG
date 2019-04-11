0b6a0566e532396a1b8c7300dde91346ba4851bb
santiago <sechava4@eafit.edu.co>
2019-04-11 12:32:28 -0500

    commit 1

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,9 @@
 #include "functions.h"
 
 
-//Name, id random y units
 
-#define CLK D2
-#define DATA D3
 
-static uint8_t prevNextCode = 0;
+volatile uint8_t pulses = 0;
 
 
 //Creacion de instancias
@@ -37,17 +34,6 @@ Trigger<int> trigger_valve = Trigger<int>('=', int(0));
 
 
 
-// A vald CW or CCW move returns 1, invalid returns 0.
-int8_t read_rotary() {
-static int8_t rot_enc_table[] = {0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0};
-
-prevNextCode <<= 2;
-if (digitalRead(DATA)) prevNextCode |= 0x02;
-if (digitalRead(CLK)) prevNextCode |= 0x01;
-prevNextCode &= 0x0f;
-
-return ( rot_enc_table[( prevNextCode & 0x0f )]);
-}
 
 
 
@@ -58,9 +44,7 @@ void setup() {
 
   Serial.begin(9600);     //Velocidad de la comunicacion serial
   //detachInterrupt(digitalPinToInterrupt(InterruptPin));
-  pinMode(CLK, INPUT);
   pinMode(CLK, INPUT_PULLUP);
-  pinMode(DATA, INPUT);
   pinMode(DATA, INPUT_PULLUP);
   encoder_rueda.attach(readTemp);  
 
@@ -88,14 +72,5 @@ void loop() {
   }
 
 }
-
-
-
-
-
-
-
-
-
 
 
